60670fec33af0b197d3148aa9ac8dddf5d3773d0
shaiksarafathima <113415020+shaiksarafathima@users.noreply.github.com>
2023-06-14 09:25:20 +0530

    packaging type war

---
+++ src/main/java/com/springbootexample/springbootexample/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.springbootexample.springbootexample.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@GetMapping("/hello")
+	String hello() {
+		return "Hi, I am working";
+	}
+}
